e8ce7915af507dbe6e243e04fa3e1e84db63bda8
ramesh622 <83131160+ramesh622@users.noreply.github.com>
2021-04-24 21:32:43 +0530

    Update ContactRepository.java

--- src/main/java/com/hackerrank/stereotypes/repository/ContactRepository.java
+++ src/main/java/com/hackerrank/stereotypes/repository/ContactRepository.java
@@ -3,6 +3,6 @@ package com.hackerrank.stereotypes.repository;
 
 import com.hackerrank.stereotypes.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+@Repository
 public interface ContactRepository extends JpaRepository<Person,Integer> {
 }
