fe3d33090c28a84b7c7097d4c4626c6f06cf4446
ramesh622 <83131160+ramesh622@users.noreply.github.com>
2021-04-24 21:32:18 +0530

    Update ContactController.java

--- src/main/java/com/hackerrank/stereotypes/controller/ContactController.java
+++ src/main/java/com/hackerrank/stereotypes/controller/ContactController.java
@@ -5,15 +5,20 @@ import com.hackerrank.stereotypes.service.ContactService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+@RestController
 public class ContactController {
+    
+    @Autowired
     ContactService contactService;
 
-    public ResponseEntity<Person> save(Person person){
+    @RequestMapping("/contact/save")
+    public ResponseEntity<Person> save(@RequestBody Person person){
         Person saved = contactService.save(person);
         return new ResponseEntity(saved, HttpStatus.CREATED);
     }
 
-    public ResponseEntity<Person> retrieve(Integer id){
+    @RequestMapping("/contact/retrieve/{id}")
+    public ResponseEntity<Person> retrieve(@Pathvariable Integer id){
         Person person = contactService.retrieve(id);
         return new ResponseEntity(person, HttpStatus.OK);
     }
